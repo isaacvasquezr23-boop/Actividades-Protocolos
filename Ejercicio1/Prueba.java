868122b63cde39a3135212927512f980ae879002
Isaac <ivasquezr@unicartagena.edu.co>
2025-10-14 02:19:21 -0500

    Ejercicio 1: clase de prueba

---
+++ Ejercicio1/Prueba.java
@@ -0,0 +1,12 @@
+package Ejercicio1;
+
+public class Prueba {
+    public static void main(String[] args) {
+        
+        Figura C = new Circulo(4);
+        Figura R = new Rectángulo(3,2);
+        
+        C.mostrarArea();
+        R.mostrarArea();
+    }
+}
