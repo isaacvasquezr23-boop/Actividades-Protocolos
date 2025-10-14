318f02f84940c61892de3624a6d3048bd87b0070
Isaac <ivasquezr@unicartagena.edu.co>
2025-10-14 02:15:46 -0500

    Ejercicio 1: clase Figura

---
+++ Ejercicio1/Figura.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+public abstract class Figura {
+    
+    public abstract double calcularArea();
+    
+    public void mostrarArea() { 
+        System.out.println("El area de la figura es: " + calcularArea());
+    }
+    }
+    
+    
+    
+
+     
+
